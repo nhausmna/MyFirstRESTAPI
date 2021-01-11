08d8cb1bb53511b0eb1eb62dfe5b12ea91446dcc
nhausmna <35944399+nhausmna@users.noreply.github.com>
2021-01-11 10:59:14 -0800

    Updated with state functions.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -20,14 +20,27 @@ class App extends Component {
         name: 'Dennis',
         job: 'Bartender',
       },
-    ]
-
-    return (
-      <div className="container">
-        <Table characterData={characters} />
-      </div>
-    )
+    ],
   }
+
+removeCharacter = index => {
+  const { characters } = this.state
+
+  this.setState({
+    characters: characters.filter((character, i) => {
+      return i !== index
+    }),
+  })
+}
+  render() {
+  const { characters } = this.state
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={this.removeCharacter} />
+    </div>
+  )
+}
 }
 
 export default App
