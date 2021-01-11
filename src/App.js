955d37f3c70814503322dc92f30f617d67c0bc18
nhausmna <35944399+nhausmna@users.noreply.github.com>
2021-01-11 11:09:51 -0800

    Updated with state functions.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form';
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
 
 removeCharacter = index => {
@@ -36,10 +20,11 @@ removeCharacter = index => {
   const { characters } = this.state
 
   return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={this.removeCharacter} />
-    </div>
-  )
+  <div className="container">
+    <Table characterData={characters} removeCharacter={this.removeCharacter} />
+    <Form />
+  </div>
+)
 }
 }
 
