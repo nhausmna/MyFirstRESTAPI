c8e0c96ebb186feb053513631e50e8685c1034a1
nhausmna <35944399+nhausmna@users.noreply.github.com>
2021-01-13 15:55:22 -0800

    Functional after Step 4.2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form';
+import axios from 'axios';
 
 class App extends Component {
   state = {
     characters: [],
   }
 
-removeCharacter = index => {
-  const { characters } = this.state
+  removeCharacter = index => {
+    const { characters } = this.state
 
-  this.setState({
-    characters: characters.filter((character, i) => {
-      return i !== index
-    }),
-  })
-}
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
   render() {
-  const { characters } = this.state
+    const { characters } = this.state
 
-  return (
-  <div className="container">
-    <Table characterData={characters} removeCharacter={this.removeCharacter} />
-    <Form handleSubmit={this.handleSubmit} />
-  </div>
-)
-}
-handleSubmit = character => {
-  this.setState({ characters: [...this.state.characters, character] })
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    )
+  }
+  handleSubmit = character => {
+    this.makePostCall(character).then(callResult => {
+      if (callResult === true) {
+        this.setState({ characters: [...this.state.characters, character] });
+      }
+    });
+  }
 
-}
+  componentDidMount() {
+    axios.get('http://localhost:5000/users')
+      .then(res => {
+        const characters = res.data.users_list;
+        this.setState({ characters });
+      })
+      .catch(function (error) {
+        //Not handling the error. Just logging into the console.
+        console.log(error);
+      });
+  }
+
+  makePostCall(character){
+    return axios.post('http://localhost:5000/users', character)
+     .then(function (response) {
+       console.log(response);
+       return (response.status === 201);
+     })
+     .catch(function (error) {
+       console.log(error);
+       return false;
+     });
+  }
 
 }
 
